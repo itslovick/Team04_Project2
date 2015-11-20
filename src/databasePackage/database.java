095ebb150f8e56e10975dc55d51e52e924331b86
itslovick <superman23504@LovickStephen.local>
2015-11-19 21:13:15 -0600

    Current Database package

---
+++ src/databasePackage/database.java
@@ -0,0 +1,284 @@
+package databasePackage;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ * @author Stephen Lovick(A00016066)
+ */
+public class database{
+//Column number for Student Table.
+public final static int id=1;
+public final static int username=2;
+public final static int fname=3;
+public final static int middle=4;
+public final static int lname=5;
+public final static int grade_level=6;
+//Column number for User Tables
+public final static int User_username=2;
+public final static int User_firstname=3;
+public final static int User_middle=4;
+public final static int User_lastname=5;
+public final static int User_account=6;
+public final static int User_password=7;
+//Column number for Teacher tables;
+public final static int Teacher_id=1;
+public final static int Teacher_username=2;
+public final static int Teacher_firstname=3;
+public final static int Teacher_middle=4;
+public final static int Teacher_lastname=5;
+//Column number for Course table
+public final static int Course_courseID=1;
+public final static int Course_department=2;
+public final static int Course_courseName=3;
+public final static int Course_hours=4;
+//Column number for Section table;
+public final static int Section_course_id=1;
+public final static int Section_id=2;
+public final static int Section_days=3;
+public final static int Section_time=4;
+public final static int Section_professor=5;
+public final static int Section_start_date=6;
+public final static int Section_end_date=7;
+//Column number for Student Stat table
+public final static int Stat_id=1;
+public final static int Stat1=2;
+public final static int Stat2=3;
+public final static int Stat3=4;
+public final static int Stat4=5;
+//Column number for Grades Table
+public final static int Grade_anum=1;
+public final static int Grade_firstname=2;
+public final static int Grade_lastname=3;
+public final static int Grade_class1=4;
+
+//Connection parameters
+private final static String url = "jdbc:mysql://ec2-54-174-90-208.compute-1.amazonaws.com:3306/";
+private final static String user = "team04";
+private final static String password = "team04";
+private final static String dbname = "Team04_Project2_Database";
+private final static String driverName = "com.mysql.jdbc.Driver";
+
+//used for all connection and disconnection
+private Connection dbCon;
+//query variables
+protected String q_user,q_fname,q_middle,q_last,q_department;
+protected int q_schoolID;
+
+//public constructor
+public database() {
+	dbCon=null;
+	q_user=null;
+	q_fname=null;
+	q_middle=null;
+	q_last=null;
+	q_department=null;
+//Accessing drivers from JAR file
+	try {
+			Class.forName(driverName);
+		} 
+	catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+}
+
+//Setters
+/*
+private void setQuser(String user){
+	q_user=user;
+}
+private void setQfname(String fname){
+	q_fname=fname;
+	//todo
+}
+private void setQmiddle(String middle){
+	q_middle=middle;
+	//todo
+}
+private void setQlast(String lname){
+	q_last=lname;
+	//todo
+}
+private void setQdepartment(String dept){
+	q_department=dept;
+	//todo
+}
+private void setQschoolID(int id){
+	q_schoolID=id;
+}
+*/
+
+//Getters
+public String getQ_user(){
+	return q_user;
+}
+public String getQ_fname(){
+	return q_fname;	
+}
+public String getQ_middle(){
+	return q_middle;
+}
+public String getQ_last(){
+	return q_last;
+}
+public String getQ_department(){
+	return q_department;
+}
+public int getQschoolID(){
+	return q_schoolID;
+}
+
+//opens connection for database
+//error handles sqlexception
+/**
+ * Opens Connection to database
+ * @param n/a
+ * @throws SQLException
+ */
+private void openConnection() throws SQLException {
+	
+		//Creating a variable for the connection called "con"
+	try{
+			dbCon = DriverManager.getConnection(url+dbname,user,password);
+			//url: jdbc:mysql://:
+			//database user:
+			//database password:		
+		}
+	catch (Exception SQLException) {
+		System.out.println(SQLException.getMessage()+"\nConnection Failed.");	
+		}
+
+}
+
+/**
+ * @return Boolean
+ * @param n/a
+ * @throws SQLException
+ * Closes connection for to database
+ */
+private void closeConnection() throws SQLException{
+		dbCon.close();
+}
+
+/**
+ * @param username, password
+ * @return String (T,S,N)
+ * @throws SQLException
+ * Test username and password against records in database
+ */
+public String validateUser(String username, String password) throws SQLException{
+	try {
+			openConnection();
+			PreparedStatement statement = dbCon.prepareStatement("Select * from User where username = ?");
+			statement.setString(1, username);
+			ResultSet result = statement.executeQuery();
+			if (result.next()&&(result.getString(User_username).equals(username)&&result.getString(User_password).equals(password)))
+				return result.getString(User_account);
+			else
+				return "N";
+
+		} 
+	catch (SQLException e) {
+			e.printStackTrace();
+			closeConnection();
+		}
+	return null;
+	}
+
+//Shows all Stats from Student Stat Table. Ends output with null value.
+//outputs records in rows.
+/**
+ * @return String
+ * @throws SQLException
+ * -Opens Connection
+ * -All records in Stats
+ * -Closes connections
+ */
+public String studentStats() throws SQLException{
+	openConnection();
+	PreparedStatement displayStats=dbCon.prepareStatement("Select * from Stat");
+	ResultSet result = displayStats.executeQuery();
+	while (result.next()) {
+		return (result.getString(Stat_id)+result.getString(Stat1)+result.getString(Stat2)+result.getString(Stat3));
+	}
+	closeConnection();
+	return null;
+}
+
+//Shows Student Stats if it anumber specified.
+/**
+ * @param id
+ * @return String
+ * @throws SQLException
+ * -Open connection
+ * -Returns record associated with id
+ * -Closes connection
+ */
+public String studentStats(int id) throws SQLException{
+	openConnection();
+	PreparedStatement displayStats=dbCon.prepareStatement("Select * from Stat where a_num = ?");
+	displayStats.setInt(1, id);
+	ResultSet result = displayStats.executeQuery();
+	while (result.next()) {
+		return (result.getString(Stat_id)+result.getString(Stat1)+result.getString(Stat2)+result.getString(Stat3));
+	}
+	closeConnection();
+	return null;
+	
+}
+
+
+/**
+ * @param user
+ * @return String
+ * @throws SQLException
+ * -Open connection
+ * -Returns user info from table.
+ * -Close connection
+ */
+public String getUserInfo(String user) throws SQLException{
+	openConnection();
+	PreparedStatement displayInfo=dbCon.prepareStatement("Select * from User where username = ?");
+	displayInfo.setString(1, user);
+	ResultSet result = displayInfo.executeQuery();
+	while (result.next()) {
+		return (result.getString(User_username)+" "+result.getString(User_firstname)+" "+result.getString(User_middle)+" "+result.getString(User_lastname)+" "+result.getString(User_account));
+	}
+	closeConnection();
+	return null;
+	
+}
+/**
+ * 
+ * @param user,fname,mi,lname,pass
+ * @throws SQLException
+ * -Open Connection
+ * -Adds User
+ * -Close Connection
+ */
+//insert new user record
+public void insertUser(String user, String fname, String mi, String lname, String pass) throws SQLException{
+	openConnection();
+	String insertQuery = " insert into users (username, firstname, middle, lastname, account type, password) values (?, ?, ?, ?, ?)";
+	PreparedStatement insertStmt = dbCon.prepareStatement(insertQuery);
+	insertStmt.setString(1, user);
+	insertStmt.setString(2, fname);
+	insertStmt.setString(3, mi);
+	insertStmt.setString(4, lname);
+	insertStmt.setString(5, pass);
+	try{
+	insertStmt.executeQuery();
+	}
+	catch(Exception SQLException){
+		SQLException.getCause();
+		SQLException.getMessage();
+		closeConnection();
+	}
+}
+
+
+
+}
